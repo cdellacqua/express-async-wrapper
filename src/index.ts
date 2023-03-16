0fa8c88181f01b0485c183bc1a1b48cbfedc9686
cdellacqua <carlo.dellacqua97@gmail.com>
2023-03-16 23:10:17 +0100

    fix callback type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import {
 	Request, Response, RequestHandler, NextFunction,
 } from 'express';
 
-export function asyncWrapper(callback: (req: Request, res: Response, next: NextFunction) => void): RequestHandler {
+export function asyncWrapper(callback: (req: Request, res: Response, next: NextFunction) => void | Promise<void>): RequestHandler {
 	return (req: Request, res: Response, next: NextFunction): Promise<void> => Promise.resolve().then(() => callback(req, res, next)).catch(next);
 }
